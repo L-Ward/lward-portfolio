f7f391d18cd2db515b6f2cb3bb967b9b5db1d351
L-Ward <leahobney@gmail.com>
2018-09-21 11:00:37 -0700

    more setup and failing at a navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+import "./Navbar.css";
+
+class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <nav>
+        
+      </nav>
+    );
+  }
+}
+
+export default Navigation;
